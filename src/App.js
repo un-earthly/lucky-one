de1f46c0fc571cba11a4e603f6d2dff9450d3205
un-earthly <vijayalamin@gmail.com>
2022-03-25 17:59:59 -0700

    bulma added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
+const element = <FontAwesomeIcon icon={faCoffee} />
 function App() {
   return (
     <div className="App">
-      <h1>Cleande up</h1>
-    </div>  
+      <h1 className='has-background-dark'>
+        Cleande up
+        <FontAwesomeIcon icon={faCoffee} />
+      </h1>
+
+    </div>
   );
 }
 
